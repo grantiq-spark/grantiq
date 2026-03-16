bb7f7fb303dc93f693d2c269514af0670dcf78a1
grantiq-spark <spark@umtr.bio>
2026-03-17 08:54:13 +0900

    feat: Slack-first OS + admin console + tool_choice hotfix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  env: {
-    NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME || "㎌움틀",
-  },
 };
 module.exports = nextConfig;
