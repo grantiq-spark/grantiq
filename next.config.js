556dbc681031bbdd868d7004f3d2d141e95cb29e
grantiq-spark <spark@umtr.bio>
2026-03-17 00:03:43 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   env: {
-    NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME || "㈜움틀",
+    NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME || "㎌움틀",
   },
 };
-
 module.exports = nextConfig;
